eca9d6e646826c547ca459e54f17a4042b9c69c6
WarrenMfg <warrenmfg@icloud.com>
2020-04-21 13:34:50 -0400

    UI present

---
+++ app.js
@@ -0,0 +1,41 @@
+// GLOBALS
+const defaultValues = {
+  firstName: 'Captain',
+  lastName: 'Obvious',
+
+};
+
+// ONLOAD
+// populate form with default values
+
+
+// EVENT HANDLERS
+// event delegation: on focus, remove default values
+
+
+// event delegation: on blur, if input is empty, add default values
+
+
+// event delegation for .names (firstName, lastName): on change
+
+
+// compare email addresses
+
+
+// confirm at least two checkboxes are selected
+
+
+/*
+The Submit Button, when clicked, will display all of the errors on the form. 
+If errors exist on the form, the form data will not be e-mailed. 
+Once all errors have been removed AND all of the default text has been replaced, 
+data entered into the form will be transmitted via e-mail. 
+The Submit Button should call the submit () event handler function.
+
+e-mail the result to your personal e-mail address.
+*/
+
+/*
+The Reset Button will reset all of the fields on the form to a blank state. 
+The Reset Button should call the reset () event handler function.
+*/
